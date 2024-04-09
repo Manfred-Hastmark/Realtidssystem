2194268b9130a90bf667b0688b4a8e5fd0b6ed92
Isak Hansson <isakhansson2000@gmail.com>
2024-04-09 08:04:56 +0200

    Format

--- part1.c
+++ part1.c
@@ -1,149 +1,146 @@
 #include <part1.h>
 
+// Functions for class ToneGenerator
 
-	
-//Functions for class ToneGenerator
-	
 int toggleDeadlinesToneGenerator(ToneGenerator* self, int unused)
 {
-	self->deadline ^= 1;
-	return self->deadline;
+    self->deadline ^= 1;
+    return self->deadline;
 }
 
 void toggleMute(ToneGenerator* self, int unused)
 {
-	self->mute ^= 0xFFFFFFFF;
+    self->mute ^= 0xFFFFFFFF;
 }
 
-int volume(ToneGenerator* self, int change) 
+int volume(ToneGenerator* self, int change)
 {
-	//Do a check so that we are in range
-	if(self->volume + change > 20 || self->volume + change < 0) return self->volume;
-	
-	self->volume += change;
-	return self->volume;
+    // Do a check so that we are in range
+    if (self->volume + change > 20 || self->volume + change < 0)
+        return self->volume;
+
+    self->volume += change;
+    return self->volume;
 }
 
 void setDAC(ToneGenerator* self, int val)
 {
-	
-	if(self->silence)
-	{
-		DAC_DATA = 0;
-		return;
-	}
-	
-	DAC_DATA = val & self->volume & self->mute; 
-	const int nextBaseline = USEC(self->period); 
-	if(self->deadline)
-	{
-		SEND(nextBaseline, nextBaseline + USEC(100), self, setDAC, val ^ 0xFFFFFFFF);
-	}
-	else
-	{
-		AFTER(nextBaseline, self, setDAC, val ^ 0xFFFFFFFF);
-	}
+
+    if (self->silence)
+    {
+        DAC_DATA = 0;
+        return;
+    }
+
+    DAC_DATA = val & self->volume & self->mute;
+    const int nextBaseline = USEC(self->period);
+    if (self->deadline)
+    {
+        SEND(nextBaseline, nextBaseline + USEC(100), self, setDAC, val ^ 0xFFFFFFFF);
+    }
+    else
+    {
+        AFTER(nextBaseline, self, setDAC, val ^ 0xFFFFFFFF);
+    }
 }
 
 void dummySetDac(ToneGenerator* self, int arr)
 {
-	int* res = (int *) arr;
-	
-	volatile int dac_data;
-	
-	int max = 0;
-	int avg = 0;
-	int avgBef = CURRENT_OFFSET();
-	for(int i = 0; i < 500; i++) 
-	{
-		int before = CURRENT_OFFSET();
-		
-		//This is the function
-		dac_data = 1 & self->volume & self->mute; 
-		volatile const int nextBaseline = USEC(self->period); 
-		if(self->deadline)
-		{
-			volatile const int t1 = nextBaseline + USEC(100);
-			volatile const int t2 = 1 ^ 0xFFFFFFFF;
-			//SEND(nextBaseline, nextBaseline + USEC(100), self, setDAC, val ^ 0xFFFFFFFF);
-		}
-		else
-		{
-			volatile const int t3 = 1 ^ 1;
-			//AFTER(nextBaseline, self, setDAC, val ^ 1);
-		}
-		
-		
-		int time = CURRENT_OFFSET() - before;
-		max = (max < time) ? time : max;
-	}
-	avg = CURRENT_OFFSET() - avgBef;
-	res[0] = USEC_OF(max);
-	res[1] = USEC_OF(avg) / 500;
-} 
+    int* res = (int*)arr;
 
+    volatile int dac_data;
 
+    int max = 0;
+    int avg = 0;
+    int avgBef = CURRENT_OFFSET();
+    for (int i = 0; i < 500; i++)
+    {
+        int before = CURRENT_OFFSET();
 
-//Functions for class BackgroundTask
+        // This is the function
+        dac_data = 1 & self->volume & self->mute;
+        volatile const int nextBaseline = USEC(self->period);
+        if (self->deadline)
+        {
+            volatile const int t1 = nextBaseline + USEC(100);
+            volatile const int t2 = 1 ^ 0xFFFFFFFF;
+            // SEND(nextBaseline, nextBaseline + USEC(100), self, setDAC, val ^ 0xFFFFFFFF);
+        }
+        else
+        {
+            volatile const int t3 = 1 ^ 1;
+            // AFTER(nextBaseline, self, setDAC, val ^ 1);
+        }
+
+        int time = CURRENT_OFFSET() - before;
+        max = (max < time) ? time : max;
+    }
+    avg = CURRENT_OFFSET() - avgBef;
+    res[0] = USEC_OF(max);
+    res[1] = USEC_OF(avg) / 500;
+}
+
+// Functions for class BackgroundTask
 
 int toggleDeadlinesBackgroundTask(BackgroundTask* self, int unused)
 {
-	self->deadline ^= 1;
-	return self->deadline;
+    self->deadline ^= 1;
+    return self->deadline;
 }
 
 int load(BackgroundTask* self, int change)
 {
-	//Do a check so we are in range
-	if(self->backgroundLoopRange + change > 24000 || self->backgroundLoopRange + change < 1000) return self->backgroundLoopRange;
-	
-	self->backgroundLoopRange += change;
-	return self->backgroundLoopRange;
+    // Do a check so we are in range
+    if (self->backgroundLoopRange + change > 24000 || self->backgroundLoopRange + change < 1000)
+        return self->backgroundLoopRange;
+
+    self->backgroundLoopRange += change;
+    return self->backgroundLoopRange;
 }
 
 void runLoad(BackgroundTask* self, int backgroundLoopRange)
 {
-	while(backgroundLoopRange--); 
-	
-	if(self->deadline)
-	{
-		SEND(USEC(1300), USEC(1300), self, runLoad, self->backgroundLoopRange);
-	}
-	else
-	{
-		AFTER(USEC(1300), self, runLoad, self->backgroundLoopRange);
-	}
+    while (backgroundLoopRange--)
+        ;
+
+    if (self->deadline)
+    {
+        SEND(USEC(1300), USEC(1300), self, runLoad, self->backgroundLoopRange);
+    }
+    else
+    {
+        AFTER(USEC(1300), self, runLoad, self->backgroundLoopRange);
+    }
 }
 
 void dummyRunLoad(BackgroundTask* self, int arr)
 {
-	int* res = (int *) arr;
-	
-	int max = 0;
-	int avg = 0;
-	int avgBef = CURRENT_OFFSET();
-	for(int i = 0; i < 500; i++) 
-	{
-		int backgroundLoopRange = res[0];
-		int before = CURRENT_OFFSET();
-		
-		
-		while(backgroundLoopRange--);
-		volatile const int deadline = USEC(1300);
-		if(self->deadline)
-		{
-			//BEFORE(deadline, self, runLoad, self->backgroundLoopRange);
-		}
-		else
-		{
-			//ASYNC(self, runLoad, self->backgroundLoopRange);
-		}
-		
-		
-		int time = CURRENT_OFFSET() - before;
-		max = (max < time) ? time : max;
-	}
-	avg = CURRENT_OFFSET() - avgBef;
-	res[0] = USEC_OF(max);
-	res[1] = USEC_OF(avg) / 500;
+    int* res = (int*)arr;
+
+    int max = 0;
+    int avg = 0;
+    int avgBef = CURRENT_OFFSET();
+    for (int i = 0; i < 500; i++)
+    {
+        int backgroundLoopRange = res[0];
+        int before = CURRENT_OFFSET();
+
+        while (backgroundLoopRange--)
+            ;
+        volatile const int deadline = USEC(1300);
+        if (self->deadline)
+        {
+            // BEFORE(deadline, self, runLoad, self->backgroundLoopRange);
+        }
+        else
+        {
+            // ASYNC(self, runLoad, self->backgroundLoopRange);
+        }
+
+        int time = CURRENT_OFFSET() - before;
+        max = (max < time) ? time : max;
+    }
+    avg = CURRENT_OFFSET() - avgBef;
+    res[0] = USEC_OF(max);
+    res[1] = USEC_OF(avg) / 500;
 }
--- part2.c
+++ part2.c
@@ -2,71 +2,74 @@
 
 void nextBeat(MusicPlayer* self, int index)
 {
-	
-	// Sets a period to the toneGenerator and turns it on.
-	self->TG.silence = 0;						
-	self->TG.period = self->notePeriods[index];
-	ASYNC(&self->TG, setDAC, 0xFFFFFFFF);
-	
-	// Sleep until it should silence the toneGenerator
-	const int toneDuration = MSEC(getBeatLenght(self->beatLength[index], self->tempo, self->silenceDuration));
-	SEND(toneDuration, toneDuration + USEC(100), self, nextSilence, (index+1) & ~32); 	// (same as (index + 1) % 32)
+
+    // Sets a period to the toneGenerator and turns it on.
+    self->TG.silence = 0;
+    self->TG.period = self->notePeriods[index];
+    ASYNC(&self->TG, setDAC, 0xFFFFFFFF);
+
+    // Sleep until it should silence the toneGenerator
+    const int toneDuration = MSEC(getBeatLenght(self->beatLength[index], self->tempo, self->silenceDuration));
+    SEND(toneDuration, toneDuration + USEC(100), self, nextSilence, (index + 1) & ~32); // (same as (index + 1) % 32)
 }
 
 void nextSilence(MusicPlayer* self, int index)
 {
-	// Turns of the tone generator
-	self->TG.silence = 1;	
+    // Turns of the tone generator
+    self->TG.silence = 1;
 
-	// Sleep until the next note
-	if(self->playing == -1)
-	{
-		const int silenceDuration = MSEC(self->silenceDuration);
-		SEND(silenceDuration, silenceDuration + USEC(100), self, nextBeat, index);
-	}
-	else
-	{
-		self->playing = index;
-	}
+    // Sleep until the next note
+    if (self->playing == -1)
+    {
+        const int silenceDuration = MSEC(self->silenceDuration);
+        SEND(silenceDuration, silenceDuration + USEC(100), self, nextBeat, index);
+    }
+    else
+    {
+        self->playing = index;
+    }
 }
 
-static inline int getBeatLenght(char c, int ms, int silenceDuration) 
+static inline int getBeatLenght(char c, int ms, int silenceDuration)
 {
-	switch(c)
-	{
-		case 'a': return (ms - silenceDuration);
-		case 'b': return ((ms<<1) - silenceDuration);	// Why has "-" higher priority than "<<" ?!
-		case 'c': return ((ms>>1) - silenceDuration);	// Why has "-" higher priority than "<<" ?!
-	}
-	// Should crash the programm;
-	return -1;
+    switch (c)
+    {
+    case 'a':
+        return (ms - silenceDuration);
+    case 'b':
+        return ((ms << 1) - silenceDuration); // Why has "-" higher priority than "<<" ?!
+    case 'c':
+        return ((ms >> 1) - silenceDuration); // Why has "-" higher priority than "<<" ?!
+    }
+    // Should crash the programm;
+    return -1;
 }
 
 void setTempo(MusicPlayer* self, int bpm)
-{	
-	self->tempo = 60000 / bpm;	
-	self->silenceDuration = self->tempo / 10;
+{
+    self->tempo = 60000 / bpm;
+    self->silenceDuration = self->tempo / 10;
 }
 
 void setPeriods(MusicPlayer* self, int arrIn)
 {
-	int* arr = (int*) arrIn;
-	for(int i = 0; i < 32; i++)
-	{
-		self->notePeriods[i] = arr[i];
-	}
+    int* arr = (int*)arrIn;
+    for (int i = 0; i < 32; i++)
+    {
+        self->notePeriods[i] = arr[i];
+    }
 }
 
 int togglePlaying(MusicPlayer* self, int unused)
 {
-	if(self->playing == -1)
-	{
-		self->playing = 0;
-	}
-	else
-	{
-		ASYNC(self, nextBeat, self->playing);	// This cannot happen before -1 :)
-		self->playing = -1;
-	}
-	return self->playing;
+    if (self->playing == -1)
+    {
+        self->playing = 0;
+    }
+    else
+    {
+        ASYNC(self, nextBeat, self->playing); // This cannot happen before -1 :)
+        self->playing = -1;
+    }
+    return self->playing;
 }
