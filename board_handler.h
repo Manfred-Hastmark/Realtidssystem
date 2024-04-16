80e167ab4725e32ffe9880484e33c1b27bccc5b9
hastmark <hastmark2001@hotmail.com>
2024-04-17 00:24:25 +0200

    Shooef

--- board_handler.h
+++ board_handler.h
@@ -24,7 +24,7 @@ typedef struct
         initObject(),                                                                                                                      \
             {                                                                                                                              \
                 DISCONNECTED,                                                                                                              \
-                0,                                                                                                                         \
+                DISCONNECTED,                                                                                                              \
                 DISCONNECTED,                                                                                                              \
                 DISCONNECTED,                                                                                                              \
                 DISCONNECTED,                                                                                                              \
